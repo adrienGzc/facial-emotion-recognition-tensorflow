c20dd5923c91debad7bce96086afc7273f87991d
Adrien Guezennec <adrien.guezennec@epitech.eu>
2020-03-13 17:58:20 +0100

    add layers in CNN

--- modules/CNN.py
+++ modules/CNN.py
@@ -31,6 +31,13 @@ class CNN:
     model.add(tf.keras.layers.MaxPooling2D(2, 2))
     model.add(tf.keras.layers.Dropout(0.5))
 
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D(2, 2))
+    model.add(tf.keras.layers.Dropout(0.25))
+
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D(2, 2))
+
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(128, activation='relu'))
 
